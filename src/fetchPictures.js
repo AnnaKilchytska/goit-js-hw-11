9e77037f3e68ec54e6b83897d08a43a6f249705a
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-01-17 23:11:06 +0200

    style adjustment, simpleLightbox and notification

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,16 +1,18 @@
+const axios = require('axios').default;
+
 const API_KEY = '32875343-08825ac1bb88acf5968fba766';
 const BASE_URL = 'https://pixabay.com/api/';
 
-//http request using async/await syntax
-async function fetchPictures() {
-  const data = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`
+///////// http request using async/await syntax ///////////
+async function fetchPictures(query, page) {
+  const pictures = await axios.get(
+    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
   );
-  const pictures = await data.json();
+  console.log(pictures);
   return pictures;
 }
 
-// http request using Promises
+////////// http request using Promises ///////////
 // function fetchPictures(query, page) {
 //   return fetch(
 //     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import fetchPictures from './fetchPictures';
 
 const formEl = document.querySelector('#search-form');
@@ -7,30 +9,71 @@ const loadMoreBtn = document.querySelector('.load-more');
 loadMoreBtn.classList.add('is-hidden');
 // console.log(formEl);
 let page = 1;
+let picturesAmount = 0;
 
 formEl.addEventListener('submit', renderPictures);
 loadMoreBtn.addEventListener('click', loadMorePictures);
 
-function renderPictures(e) {
-  e.preventDefault();
-  console.log(e);
-  console.log(formEl.elements[0].value);
-  fetchPictures(formEl.elements[0].value, page).then(
-    createInterfaceAfterFirstQuery
-  );
+const lightbox = new SimpleLightbox('.gallery a', {
+  overlay: true,
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// const something = gallery.getBoundingClientRect();
+
+// console.log(something.height);
+
+// window.scrollBy({
+//   top: something.height * 2,
+//   behavior: 'smooth',
+// });
+
+////// Promise syntax /////
+// function renderPictures(e) {
+//   e.preventDefault();
+//   console.log(e);
+//   console.log(formEl.elements[0].value);
+//   fetchPictures(formEl.elements[0].value, page).then(
+//     createInterfaceAfterFirstQuery
+//   );
+// }
+
+////// async/await syntax ////////
+async function renderPictures(e) {
+  try {
+    e.preventDefault();
+    // console.log(formEl.elements[0].value);
+    const pictures = await fetchPictures(formEl.elements[0].value, page);
+    createInterfaceAfterFirstQuery(pictures);
+    // console.log(pictures);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function createInterfaceAfterFirstQuery(data) {
   gallery.innerHTML = '';
-  console.log(data.hits);
+  // console.log('pictures amount:', data.data.hits.length);
+  picturesAmount = data.data.hits.length;
+  // console.log('this is variable', picturesAmount);
 
-  if (data.hits.length !== 0) {
+  if (data.data.hits.length !== 0) {
     gallery.innerHTML = '';
     page = 1;
     const markup = createMarkup(data);
     gallery.insertAdjacentHTML('beforeend', markup);
     loadMoreBtn.classList.remove('is-hidden');
     page += 1;
+    lightbox.refresh();
   } else {
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
@@ -39,10 +82,10 @@ function createInterfaceAfterFirstQuery(data) {
 }
 
 function createMarkup(data) {
-  const picturesArr = data.hits;
+  const picturesArr = data.data.hits;
   const markup = picturesArr
     .map(item => {
-      return `<div class="photo-card">
+      return `<a href="${item.largeImageURL}" class="photo-link"><div class="photo-card">
           <img class="card-image" src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
           <div class="info">
             <p class="info-item">
@@ -58,20 +101,41 @@ function createMarkup(data) {
               <b>Downloads: </b>${item.downloads}
             </p>
           </div>
-        </div>`;
+        </div></a>`;
     })
     .join('');
   return markup;
 }
 
-function loadMorePictures() {
-  fetchPictures(formEl.elements[0].value, page)
-    .then(createMarkup)
-    .then(markup => {
-      gallery.insertAdjacentHTML('beforeend', markup);
-      console.log(page);
-      page += 1;
-    });
+/////// Promises /////////
+// function loadMorePictures() {
+//   fetchPictures(formEl.elements[0].value, page)
+//     .then(createMarkup)
+//     .then(markup => {
+//       gallery.insertAdjacentHTML('beforeend', markup);
+//       console.log(page);
+//       page += 1;
+//     });
+// }
+
+///// async/await //////
+async function loadMorePictures() {
+  try {
+    const pictures = await fetchPictures(formEl.elements[0].value, page);
+    // console.log('pictures:', pictures);
+    const markup = createMarkup(pictures);
+    // console.log('this is pictures', pictures.data.hits.length);
+    gallery.insertAdjacentHTML('beforeend', markup);
+    // console.log(page);
+    picturesAmount += pictures.data.hits.length;
+    getNotification(picturesAmount);
+    page += 1;
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// use async syntax
+function getNotification(number) {
+  Notify.info(`Hooray! We found ${number} images.`);
+}
